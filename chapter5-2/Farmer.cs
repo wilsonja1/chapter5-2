347fbc636fb5fcbb48363e02c93a293351ec1f0b
Jack Wilson <battlejackmaster@gmail.com>
2018-07-13 15:12:31 -0700

    Add project files.

---
+++ chapter5-2/Farmer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter5_2
+{
+    class Farmer
+    {
+        public int BagsOfFeed;
+        public const int FeedMultiplier = 30;
+
+        private int numberOfCows;
+
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * FeedMultiplier;
+            }
+        }
+    }
+}
